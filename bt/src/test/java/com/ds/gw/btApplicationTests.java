94db628dba1ca8a6caeb7059487286c3f6b8ab13
khy <ajjang687@gmail.com>
2023-04-26 09:58:10 +0900

    0426 commit

--- bt/src/test/java/com/ds/gw/btApplicationTests.java
+++ bt/src/test/java/com/ds/gw/btApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GroupwareApplicationTests {
+class btApplicationTests {
 
 	@Test
 	void contextLoads() {
--- pt/src/main/java/com/ds/gw/ptApplication.java
+++ pt/src/main/java/com/ds/gw/ptApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GroupwareApplication {
+public class ptApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GroupwareApplication.class, args);
+		SpringApplication.run(ptApplication.class, args);
 	}
 
 }
